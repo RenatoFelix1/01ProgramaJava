5034265e12039226ef26e3df107f540913cbd638
R3n4t0Fx <R3n4t0Fx@DESKTOP-KKM3OU6>
2023-03-31 19:18:09 -0300

    criação do projeto

---
+++ projetoincial/src/projetoincial/primeiraclasse.java
@@ -0,0 +1,9 @@
+package projetoincial;
+
+public class primeiraclasse {
+	/*main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("meu primeiro programa em java");
+	}
+	
+}
