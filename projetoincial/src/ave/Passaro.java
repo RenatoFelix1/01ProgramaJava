2372b0d23716e58981f6fbc388a4a53fb7066cd3
R3n4t0Fx <R3n4t0Fx@DESKTOP-KKM3OU6>
2023-04-10 17:29:57 -0300

    criação da classe Date e adicionando campo de data de nascimento

--- projetoincial/src/ave/Passaro.java
+++ projetoincial/src/ave/Passaro.java
@@ -3,8 +3,20 @@ package ave;
 public class Passaro {
 
 	private String name;
-	
+
 	private String color;
+	
+	private int idade;
+    
+	private Date datadenascimento;
+
+	public Date getDatadenascimento() {
+		return datadenascimento;
+	}
+
+	public void setDatadenascimento(Date datadenascimento) {
+		this.datadenascimento = datadenascimento;
+	}
 
 	public String getName() {
 		return name;
@@ -20,6 +32,14 @@ public class Passaro {
 
 	public void setColor(String color) {
 		this.color = color;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	
 }
